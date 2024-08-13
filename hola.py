3c873d851679c21861530359e7ca6cc843ac8986
Stefania Posada B <stefania.posada.spb@gmail.com>
2024-08-13 12:00:49 -0500

    Función suma

--- hola.py
+++ hola.py
@@ -1,1 +1,18 @@
-print "hola mundo"
+import webbrowser
+
+def abrir():
+    webbrowser.open("https://www.youtube.com/watch?v=mCdA4bJAGGk")
+
+
+def suma(a,b):
+    abrir()
+    return a + b
+
+
+print("hola mundo")
+
+V1 = int(input("Valor 1: "))
+V2 = int(input("Valor 2: "))
+
+print("Suma: " + str(suma(V1,V2)))
+
